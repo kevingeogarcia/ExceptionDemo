80775c164049ecd20cb8cd80a556f64964541a7b
kevingeogarcia <kevingeogarcia@gmail.com>
2023-08-26 12:07:48 -0600

    Add BussinesException to controller

--- src/main/java/demo/kevingeo/ExceptionDemo/controller/ApiController.java
+++ src/main/java/demo/kevingeo/ExceptionDemo/controller/ApiController.java
@@ -1,26 +1,31 @@
 package demo.kevingeo.ExceptionDemo.controller;
 
+import demo.kevingeo.ExceptionDemo.custom.exceptions.BussinesException;
+import demo.kevingeo.ExceptionDemo.custom.exceptions.CustomException;
+import demo.kevingeo.ExceptionDemo.response.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import demo.kevingeo.ExceptionDemo.custom.exceptions.CustomException;
-import demo.kevingeo.ExceptionDemo.response.Response;
-
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-	
-	@GetMapping("/exception")
-	public ResponseEntity<Response> exception () throws Exception{
-		throw new Exception("Mensaje desde la clase generica de java: Exception!");
-	}
-	
-	
-	@GetMapping("/custom-exception")
-	public ResponseEntity<Response> customException () throws CustomException{
-		throw new CustomException("Mensaje personalizado desde la clase: CustomException!");
-	}
+
+    @GetMapping("/exception")
+    public ResponseEntity<Response> exception() throws Exception {
+        throw new Exception("Mensaje desde la clase generica de java: Exception!");
+    }
+
+
+    @GetMapping("/custom-exception")
+    public ResponseEntity<Response> customException() throws CustomException {
+        throw new CustomException("Mensaje personalizado desde la clase: CustomException!");
+    }
+
+    @GetMapping("/custom-bussines-exception")
+    public ResponseEntity<Response> customBussinesException() throws CustomException {
+        throw new BussinesException("Mensaje personalizado desde la clase: BussinesException!");
+    }
 }
--- src/main/java/demo/kevingeo/ExceptionDemo/controller/ExceptionHendlingController.java
+++ src/main/java/demo/kevingeo/ExceptionDemo/controller/ExceptionHendlingController.java
@@ -1,25 +1,29 @@
 package demo.kevingeo.ExceptionDemo.controller;
 
+import demo.kevingeo.ExceptionDemo.custom.exceptions.BussinesException;
+import demo.kevingeo.ExceptionDemo.custom.exceptions.CustomException;
+import demo.kevingeo.ExceptionDemo.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import demo.kevingeo.ExceptionDemo.custom.exceptions.CustomException;
-import demo.kevingeo.ExceptionDemo.response.Response;
-
 @ControllerAdvice
 public class ExceptionHendlingController {
 
-	@ExceptionHandler(value = {CustomException.class})
-	public ResponseEntity<Response> demoCustomException(CustomException ex){
-		return new ResponseEntity<Response>( new Response("99", "Mensaje personalizado!" , ex.getMessage()), HttpStatus.OK);
-	}
-	
-	
 
-	@ExceptionHandler(value = {Exception.class})
-	public ResponseEntity<Response> demoCustomException(Exception ex){
-		return new ResponseEntity<Response>( new Response("99", "Mensaje personalizado!" , ex.getMessage()), HttpStatus.OK);
-	}
+    @ExceptionHandler(value = {BussinesException.class})
+    public ResponseEntity<Response> demoCustomException(BussinesException ex) {
+        return new ResponseEntity<Response>(new Response("99", "Mensaje personalizado!", ex.getMessage()), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(value = {CustomException.class})
+    public ResponseEntity<Response> demoCustomException(CustomException ex) {
+        return new ResponseEntity<Response>(new Response("99", "Mensaje personalizado!", ex.getMessage()), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Response> demoCustomException(Exception ex) {
+        return new ResponseEntity<Response>(new Response("99", "Mensaje personalizado!", ex.getMessage()), HttpStatus.OK);
+    }
 }
